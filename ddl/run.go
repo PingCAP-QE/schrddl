f43e549185dfd6eb3a25de22f0ebdf7088f5fe05
Lynn <zimu_xia@126.com>
2021-06-12 21:07:46 +0800

    ddl: filter truncated error and "bad number" error for modify column (#3)
    * *: add filter truncate error
    
    * ddl: update error msgs
    
    * ddl: filter bad number error

--- ddl/run.go
+++ ddl/run.go
@@ -120,7 +120,7 @@ func dmlIgnoreError(err error) bool {
 		strings.Contains(errStr, "converting driver.Value type") || strings.Contains(errStr, "column specified twice") ||
 		strings.Contains(errStr, "Out of range value for column") || strings.Contains(errStr, "Unknown column") ||
 		strings.Contains(errStr, "column has index reference") || strings.Contains(errStr, "Data too long for column") ||
-		strings.Contains(errStr, "Data Truncated") || strings.Contains(errStr, "no rows in result set") ||
+		strings.Contains(errStr, "Data truncated") || strings.Contains(errStr, "no rows in result set") ||
 		strings.Contains(errStr, "Truncated incorrect") || strings.Contains(errStr, "Data truncated for column") ||
 		// eg: For Incorrect tinyint value, Incorrect data value...
 		strings.Contains(errStr, "Incorrect") ||
@@ -166,6 +166,8 @@ func ddlIgnoreError(err error) bool {
 		strings.Contains(errStr, "value is out of range") ||
 		strings.Contains(errStr, "Unsupported modify charset from") ||
 		strings.Contains(errStr, "Unsupported modifying collation of column") ||
+		strings.Contains(errStr, "Data truncated") ||
+		strings.Contains(errStr, "Bad Number") ||
 		// eg: For v"BLOB/TEXT column '319de167-6d2e-4778-966c-60b95103a02c' used in key specification without a key length"
 		strings.Contains(errStr, "used in key specification without a key length") {
 		fmt.Println(errStr)
@@ -181,7 +183,7 @@ func ddlIgnoreError(err error) bool {
 		strings.Contains(errStr, "converting driver.Value type") || strings.Contains(errStr, "column specified twice") ||
 		strings.Contains(errStr, "Out of range value for column") || strings.Contains(errStr, "Unknown column") ||
 		strings.Contains(errStr, "column has index reference") || strings.Contains(errStr, "Data too long for column") ||
-		strings.Contains(errStr, "Data Truncated") || strings.Contains(errStr, "no rows in result set") {
+		strings.Contains(errStr, "Data truncated") || strings.Contains(errStr, "no rows in result set") {
 		return true
 	}
 	return false
