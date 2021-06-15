e7a918689142133827c7c7b8d9e3465fce2c744d
Lynn <zimu_xia@126.com>
2021-06-15 11:26:05 +0800

    ddl: filter "Data Too Long" and "cannot convert" errors (#5)

--- ddl/run.go
+++ ddl/run.go
@@ -169,6 +169,8 @@ func ddlIgnoreError(err error) bool {
 		strings.Contains(errStr, "Unsupported modifying collation of column") ||
 		strings.Contains(errStr, "Data truncated") ||
 		strings.Contains(errStr, "Bad Number") ||
+		strings.Contains(errStr, "cannot convert")||
+		strings.Contains(errStr, "Data Too Long")||
 		// eg: For v"BLOB/TEXT column '319de167-6d2e-4778-966c-60b95103a02c' used in key specification without a key length"
 		strings.Contains(errStr, "used in key specification without a key length") {
 		fmt.Println(errStr)
