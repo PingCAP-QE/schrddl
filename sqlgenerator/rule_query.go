3a004e05ed6d487c38a4ba7842ec30d684351d6c
wjhuang2016 <huangwenjun1997@gmail.com>
2024-04-22 17:39:50 +0800

    refine limit
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- sqlgenerator/rule_query.go
+++ sqlgenerator/rule_query.go
@@ -702,7 +702,7 @@ var OrderBy = NewFn(func(state *State) Fn {
 })
 
 var Limit = NewFn(func(state *State) Fn {
-	return Strs("limit", RandomNum(2000000, 10000000))
+	return Strs("limit", RandomNum(10000000, 100000000))
 
 	//return Strs("limit", RandomNum(1000000, 2147483646))
 })
