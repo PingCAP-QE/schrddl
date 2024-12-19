5aa1a6d9276aa239a5b53100d0156d92d974edb7
wjhuang2016 <huangwenjun1997@gmail.com>
2024-12-19 17:44:55 +0800

    refine
    Signed-off-by: wjhuang2016 <huangwenjun1997@gmail.com>

--- framework/run.go
+++ framework/run.go
@@ -124,6 +124,7 @@ var dmlIgnoreList = []string{
 	"Unsupported expression type MysqlBit",
 	"Unexpected missing column",
 	"strconv.Atoi",
+	"Unsupported tidb_enable_dist_task setting",
 	//"other error for mpp stream",
 
 	"Can't find a proper physical plan for this query",
